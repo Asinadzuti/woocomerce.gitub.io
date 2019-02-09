1aece02c4cfe6855004bfe6e89797dff608a2cf1
Asinadzuti <44137543+Asinadzuti@users.noreply.github.com>
2019-02-09 17:17:50 +0300

    first

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package woocomerce
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+</div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
